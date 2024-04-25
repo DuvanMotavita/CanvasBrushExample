49e9463b59f302c5132958b5fb522f0b6b592ced
DuvanMotavita <duvanfel1@yahoo.com.co>
2024-04-24 23:01:17 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+//Var declaration
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+//Listener to wait for resize and add some responsive logic
+window.addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+//Mouse object to use on mouse events
+const mouse = {
+  x: undefined,
+  y: undefined,
+};
+//Listerner to do change render point on mouse click
+canvas.addEventListener("click", (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  drawCircle();
+});
+//Function to wait for mouse brush movement
+canvas.addEventListener("mousemove", (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  drawCircle();
+});
+//Function to draw circle on canvas
+function drawCircle() {
+  ctx.fillStyle = "blue";
+  ctx.beginPath();
+  ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
+  ctx.fill();
+}
+//Function instance
+drawCircle();
